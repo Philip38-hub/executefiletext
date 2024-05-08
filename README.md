1a5dffc936e77ac9c566ecdb80f1108d61bfcd2b
oumaphilip01@gmail.com <oumaphilip01@gmail.com>
2024-05-08 10:10:13 +0300

    execute program in a .txt file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Scope
 This program reads a .txt(input.txt) file with an uncomplete program to print hello world!
 The incomplete program is completed, executed within the program and its ouput is diplayed in another .txt file(output.txt) 
+
+### imports
+os to read and write from input.txt and output.txt respectively
+os/exe to execute the formated program
+strings to replace the uwanted strings with the wanted so that the program is formatted effectively
--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	program := strings.ReplaceAll(string(file), "package", "package main")
 	program = strings.ReplaceAll(program, "import", "import \"fmt\"")
 
-	temp := "temp.go"
+	temp := "temp.go" // create a temporary file to hold the program while you run it.
 	os.WriteFile(temp, []byte(program), 0644)
-	defer os.Remove(temp)
+	defer os.Remove(temp) // remove file from your files to avoid errors
 	cmd := exec.Command("go", "run", temp)
-	out, _ := cmd.CombinedOutput()
+	out, _ := cmd.CombinedOutput() // combine output
 	os.WriteFile("output.txt", []byte(out), 0644)
 }
